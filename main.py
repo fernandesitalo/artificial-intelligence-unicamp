976a812e9f0157b44f673c0afbb488b0784927a7
David Almeida <davimelo72@gmail.com>
2022-03-27 21:48:50 -0300

    Add. Criação de ambiente de execução virtual, inicialização do projeto no PyCharm e criação da classe IDA*.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from read_and_process_input import *
 from geometric_functions import *
+from IdaStar import IdaStar
 
 def main():
 	archive = "input.txt"
 	obj_input = read_and_process_input(archive)
 	print(obj_input.graph)
+
+	test = IdaStar(obj_input.graph)
 
 
 if __name__ == '__main__':
